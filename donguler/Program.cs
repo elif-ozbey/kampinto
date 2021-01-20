fc33777fe8bd70edfa032755d3b7249c1e89cf6c
Elif Ozbey <eozbey2002@hotmail.com>
2021-01-20 19:30:45 +0300

    Proje dosyası ekle.

---
+++ donguler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazilimci yetistirme";
+            string kurs2 = "Temel Kurs";
+            string kurs3 = "Java kursu";
+
+            string[] kurslar = new string[] { "Yazilimci yetistirme", "Temel Kurs", "Java kursu", "C#"};
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar [i]);
+            }
+            Console.WriteLine("for bitti");
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("foreach bitti");
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
---
+++ kampinto/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace kampinto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategorietiketi = "Kategori";
+            Console.WriteLine("kategoriler");
+            int ogrencisayisi = 32000;
+            double faizoani = 1.45;
+            bool sistemegiris = false;
+            double dolarbugun = 7.35;
+            double dolardun = 7.45;
+
+            if (dolarbugun > dolardun)
+            {
+                Console.WriteLine("artis butonu");
+            }
+
+            else if (dolarbugun < dolardun)
+            {
+                Console.WriteLine("azalis butonu");
+            }
+
+            else
+            {
+                Console.WriteLine("durum degismedi");
+            }
+
+            if (sistemegiris==true)
+            {
+                Console.WriteLine("sistem ayarlari butonu");
+            }
+            else 
+            {
+                Console.WriteLine("sisteme giris yap");
+            }
+
+        }
+    }
+}
